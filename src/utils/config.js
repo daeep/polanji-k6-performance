5b6726537df6f8bcf28ecd8fe52908c1b668d253
antoniojn81 <antoniojn@hotmail.com>
2025-12-04 12:52:31 -0600

    second

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,23 @@
-import json from '../../config/testConfig.json';
-export default json;
+export default {
+  "baseURL": "https://api.polanji.com",
+  "credentials": {
+    "username": "performancetest09@gmail.com",
+    "password": "user123456"
+  },
+  "courseId": 19,
+  "userId": 13,
+  "loadTest": {
+    "stages": [
+      { "duration": "30s", "target": 5 },
+      { "duration": "30s", "target": 10 },
+      { "duration": "30s", "target": 0 }
+    ]
+  },
+  "stressTest": {
+    "stages": [
+      { "duration": "30s", "target": 10 },
+      { "duration": "30s", "target": 20 },
+      { "duration": "30s", "target": 0 }
+    ]
+  }
+};
